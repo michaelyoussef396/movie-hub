443b0b80f82ad63e5635b9901b1ede3203b486dc
Michael Youssef <michaelyoussef396@gmail.com>
2025-06-17 17:42:35 +1000

    track search term fequency with appwrite backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Search from './components/Search.jsx'
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
 import {useDebounce} from "react-use";
+import {updateSearchCount} from "./appwrite.js";
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 
 const API_KEY = import.meta.env.VITE_TMBD_API_KEY
@@ -49,6 +50,10 @@ function App() {
           }
 
           setMovieList(data.results || []);
+
+          if(query && data.results.length > 0) {
+              await updateSearchCount(query, data.results[0]);
+          }
       } catch (error) {
           console.error(`Error fetching movies: ${error}`);
           setErrorMessage('Error fetching movies. Please try again later.');
